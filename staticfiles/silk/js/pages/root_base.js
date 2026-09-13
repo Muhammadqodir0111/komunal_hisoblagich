57735cf161ad1fcc40ebe48cf410409a33b75335
Muhammadqodir0111 <muhammadqodirmominov55@gmail.com>
2026-09-13 23:06:37 +0500

    Update gitignore

--- staticfiles/silk/js/pages/root_base.js
+++
@@ -1,8 +0,0 @@
-function initFilterButton() {
-    $('#filter-button').click(function () {
-        $(this).toggleClass('active');
-        $('body').toggleClass('cbp-spmenu-push-toleft');
-        $('#cbp-spmenu-s2').toggleClass('cbp-spmenu-open');
-        initFilters();
-    });
-}
